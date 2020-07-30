7e21d196c1035ea7953a02e431d5c1baa2ac5bd9
Vladimir Ivanin <ivaninvv@mail.ru>
2020-07-30 17:55:45 +0300

    перехват ошибки

--- src/modules/searchAction.js
+++ src/modules/searchAction.js
@@ -4,7 +4,7 @@ export default (input, country) => {
   const kladrUrl = 'https://kladr.insales.ru'
   const url = `${kladrUrl}/fulltext_search.json?q=${input}&country=${country}&with_parent=1`
 
-  return new Promise(resolve => {
+  return new Promise((resolve, reject) => {
     if (input.length < 2) {
       return resolve([])
     }
@@ -15,7 +15,11 @@ export default (input, country) => {
     })
     .then(response => response.json())
     .then(data => {
-      resolve(data);
+      if (data.error) {
+        reject(data.error);
+      }else{
+        resolve(data);
+      }
     })
     .catch((err) => {
       console.warn(err);
